1a21d12c84b0db56ff3a466862ac272c01ce77d1
johpetsc <32446989+johpetsc@users.noreply.github.com>
2018-11-30 20:59:59 -0200

    CRUD quase pronto

--- bd.py
+++ bd.py
@@ -1,5 +1,6 @@
 import pymysql
 import os
+import sys
 
 connection = pymysql.connect(
     host='localhost',
@@ -8,88 +9,193 @@ connection = pymysql.connect(
     db='trabalhobd',
 )
 
+def stop():
+    print("\n\n\n\t\tPressione Enter para continuar.")
+    sys.stdin.read(1)
+
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
 def create():
-    cpf = input("cpf: ")
-    p_nome = input("Nome: ")
-    u_nome = input("Sobrenome: ")
-    data_nascimento = input("Data: ")
-    idade = input("Idade: ")
-    endereco = input("Endereco: ")
+    tab = input("\n\n\n\tNome da tabela onde será feita a operação INSERT: ")
+    cls()
 
-    try:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO pessoa (cpf, p_nome, u_nome, data_nascimento, idade, endereco) VALUES (%s, %s, %s, %s, %s, %s);"
-            try:
-                cursor.execute(sql, (cpf, p_nome, u_nome, data_nascimento, idade, endereco))
-                print("Pessoa adicionada com sucesso.")
-            except:
-                print("Falha ao adicionar pessoa.")
-        connection.commit()
-    finally:
-        connection.close()
+    if tab == "pessoa":
+        cpf = input("\tCPF: ")
+        nome = input("\tNome: ")
+        sobrenome = input("\tSobrenome: ")
+        data = input("\tData: ")
+        idade = input("\tIdade: ")
+        endereco = input("\tEndereço: ")  
+        try:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO pessoa (cpf, p_nome, u_nome, data_nascimento, idade, endereco) VALUES (%s, %s, %s, %s, %s, %s);"
+                try:
+                    cursor.execute(sql, (cpf, nome, nome, data, idade, endereco))
+                    print("\n\tPessoa adicionada com sucesso.")
+                    stop()
+                except:
+                    print("\n\tFalha ao adicionar pessoa.")
+                    stop()
+            connection.commit()
+        finally:
+            connection.close()
 
 def read():
-    try:
-        with connection.cursor() as cursor:
-            sql = "SELECT * FROM pessoa"
-            try:
-                cursor.execute(sql)
-                result = cursor.fetchall()
-    
-                print("Cpf\tNome\tSobrenome\tData\tIdade\tEndereco")
-                print("---------------------------------------------------------------------------")
-                for row in result:
-                    print(row)
-            except:
-                print("Falha ao ler tabela.")
-        connection.commit()
-    finally:
-        connection.close()
+    tab = input("\n\n\n\tNome da tabela para leitura: ")
+    print("\n\tVer tabela completa - 1\n\n\tUsar condição WHERE - 2")
+    opc = input("Opção: ")
+    cls()
+
+    if opc == "1":
+        try:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM " + tab + ";"
+                try:
+                    cursor.execute(sql)
+                    result = cursor.fetchall()
+            
+                    print("\n\nCpf\tNome\tSobrenome\tData\tIdade\tEndereco")
+                    print("---------------------------------------------------------------------------")
+                    for row in result:
+                        print(row)
+                    stop()
+                except:
+                    print("\n\tFalha ao ler tabela.")
+                    stop()
+            connection.commit()
+        finally:
+            connection.close()
+    else:
+        cond = input("\n\n\tEscreva a condição usando sintaxe SQL.\n\nSELECT * FROM " + tab + "\nWHERE ")
+
+        try:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM " + tab + " WHERE " + cond + ";"
+                try:
+                    cursor.execute(sql)
+                    result = cursor.fetchall()
+            
+                    print("\n\nCpf\tNome\tSobrenome\tData\tIdade\tEndereco")
+                    print("---------------------------------------------------------------------------")
+                    for row in result:
+                        print(row)
+                    stop()
+                except:
+                    print("\n\tFalha ao ler tabela.")
+                    stop()
+            connection.commit()
+        finally:
+            connection.close()
 
 def update():
-    try:
-        with connection.cursor() as cursor:
-            sql = "UPDATE pessoa SET p_nome=%s, u_nome=%s WHERE cpf = %s"
+    tab = input("\n\n\n\tNome da tabela onde será feita a operação UPDATE: ")
+    frase = "\n\tId da tabela " + tab + " a ser atualizado: "
+    n_id = input(frase)
+    cls()
+
+    if tab == "pessoa":
+        print("\n\n\n\tSelecione o que atualizar:")
+        print("\n\tCPF - 1\n\tNome - 2\n\tSobrenome - 3\n\tData - 4\n\tIdade - 5\n\tEndereço - 6\n\tTudo - 7\n\tFim - 0")
+        opc = input("Opção: ")
+        cls()
+        print("\n\n\n\t")
+        if opc == "1":
+            cpf = input("\tCPF: ")
+            frase = "cpf='" + cpf + "'"
+        elif opc == "2":
+            nome = input("\tNome: ")
+            frase = "p_nome='" + nome + "'"
+        elif opc == "3":
+            sobrenome = input("\tSobrenome: ")
+            frase = "u_nome='" + sobrenome + "'"
+        elif opc == "4":
+            data = input("\tData: ")
+            frase = "data_nascimento='" + data + "'"
+        elif opc == "5":
+            idade = input("\tIdade: ")
+            frase = "idade=" + idade
+        elif opc == "6":
+            endereco = input("\tEndereço: ")
+            frase = "endereco='" + endereco + "'"
+        elif opc == "7":
+            cpf = input("\tCPF: ")
+            nome = input("\tNome: ")
+            sobrenome = input("\tSobrenome: ")
+            data = input("\tData: ")
+            idade = input("\tIdade: ")
+            endereco = input("\tEndereço: ")
+            frase = "cpf='" + cpf + "', p_nome='" + nome + "', u_nome='" + sobrenome + "', data_nascimento='" + data + "', idade=" + idade + ", endereco='" + endereco + "'"
+           
+        if opc != "0":
             try:
-                cursor.execute(sql, ('Joao', 'Doido', 1))
-                print("Pessoa atualizada com sucesso.")
-            except:
-                print("Falha ao atualizar pessoa.")
-    
-        connection.commit()
-    finally:
-        connection.close()
+                with connection.cursor() as cursor:
+                    sql = "UPDATE " + tab + " SET " + frase + " WHERE id_" + tab + "= %s;"
+                    try:
+                        cursor.execute(sql, (n_id))
+                        print("\n\t" + tab.capitalize() + " atualizada com sucesso.")
+                        stop()
+                    except:
+                        print("\n\tFalha ao atualizar " + tab + ".")
+                        stop()
+                
+                connection.commit()
+            finally:
+                connection.close()
 
 def delete():
-    try:
-        with connection.cursor() as cursor:
-            sql = "DELETE FROM pessoa WHERE cpf = %s"
-            try:
-                cursor.execute(sql, (1))
-                print("Pessoa deletada com sucesso.")
-            except:
-                print("Falha ao deletar pessoa.")
-    
-        connection.commit()
-    finally:
-        connection.close()
+    tab = input("\n\n\n\tNome da tabela onde será feita a operação DELETE: ")
+    print("\n\tDeletar a tabela - 1\n\n\tDeletar linha na tabela - 2")
+    opc = input("Opção: ")
+    cls()
 
-cls()
+    if opc == "2":
+        frase = "\n\tId do(a) " + tab + " a ser deletado: "
+        n_id = input(frase)
 
-print("\t\t\tCRUD para o banco de dados.\n\n")
-print("\tCREATE - 1 \n\n\tREAD - 2 \n\n\tUPDATE - 3 \n\n\tDELETE - 4 \n\n\tSair - 0")
-opc = input("\n\n\tOpção:")
+        try:
+            with connection.cursor() as cursor:
+                sql = "DELETE FROM " + tab + " WHERE id_" + tab + "= %s;"
+                try:
+                    cursor.execute(sql, (n_id))
+                    print("\n\t" + tab.capitalize() + " deletada com sucesso.")
+                    stop()
+                except:
+                    print("\n\tFalha ao deletar " + tab + ".")
+                    stop()        
+            connection.commit()
+        finally:
+            connection.close()
+    else:
+        try:
+            with connection.cursor() as cursor:
+                sql = "DROP TABLE IF EXISTS " + tab.upper() + ";"
+                try:
+                    cursor.execute(sql, (n_id))
+                    print("\n\tTabela" + tab + " deletada com sucesso.")
+                    stop()
+                except:
+                    print("\n\tFalha ao deletar tabela" + tab + ".")
+                    stop()
+        
+            connection.commit()
+        finally:
+            connection.close()
+opc = 10
+while opc != "0":
+    cls()
 
-cls()
+    print("\t\t\tCRUD para o banco de dados.\n\n")
+    print("\tCREATE - 1 \n\n\tREAD - 2 \n\n\tUPDATE - 3 \n\n\tDELETE - 4 \n\n\tSair - 0")
+    opc = input("Opção: ")
 
-if opc == "1":
-    create()
-elif opc == "2":
-    read()
-elif opc == "3":
-    update()
-elif opc == "4":
-    delete()
+    cls()
+
+    if opc == "1":
+        create()
+    elif opc == "2":
+        read()
+    elif opc == "3":
+        update()
+    elif opc == "4":
+        delete()
